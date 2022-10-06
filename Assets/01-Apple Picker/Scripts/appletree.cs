c9954b7da03a1e67e5a2a72cb6b756f472f539b0
dusty1312 <113940912+dusty1312@users.noreply.github.com>
2022-10-06 14:08:33 -0400

    tree drops apples

--- Assets/01-Apple Picker/Scripts/appletree.cs
+++ Assets/01-Apple Picker/Scripts/appletree.cs
@@ -1,51 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Appletree : MonoBehaviour
-{
+
+public class Appletree : MonoBehaviour {  
+
     [Header("Set in Inspector")]
     // Prefab for instantiating apples
     public GameObject ApplePrefab;
     public GameObject greenapplePrefab;
     // Speed at which the AppleTree moves
-    public float speed = 5f;
+    public float speed = 1f;
     // Distance where AppleTree turns around
-    public float leftAndRightEdge = 20f;
+    public float leftAndRightEdge = 10f;
     // Chance that the AppleTree will change directions
-    public float chanceToChangeDirections = 0.2f;
+    public float chanceToChangeDirections = 0.1f;
     // Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrops = 1f;
-    void Start()
-    {
-        Invoke("DropApple", 2f);
+
+    void Start(){
+        InvokeRepeating("DropApple", 1f,secondsBetweenAppleDrops);
     }
-    void DropApple()
-    { // b
-        GameObject apple = Instantiate<GameObject>(applePrefab); // c
-        apple.transform.position = transform.position; // d
-        Invoke("DropApple", secondsBetweenAppleDrops); // e
-    }
+
     void Update()
     {
         Vector3 pos = transform.position; // b
         pos.x += speed * Time.deltaTime;
-        pos.x += 1.0f * 0.04f;
-        pos.x += 0.04f;
         transform.position = pos;
+        pos.x += speed * Time.deltaTime;
+        pos.x += 1.0f * 0.02f;
+        pos.x += 0.02f;
 
         if (pos.x < -leftAndRightEdge)
         {
-            speed = Mathf.Abs(speed); // Move right // b
-        }
+
+            speed = Mathf.Abs(speed);
+        } // Move right // b 
+
         else if (pos.x > leftAndRightEdge)
         { // c
             speed = -Mathf.Abs(speed); // Move lef
         }
-        void FixedUpdate()
-        {
-            if (Random.value < chanceToChangeDirections)
-            { // b
-                speed *= -1; // Change direction
-            }
+    }
+         
+    
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        { // b
+            speed *= -1; // Change direction
         }
+    }
+
+    void DropApple()
+    { // b
+        GameObject Apple = Instantiate<GameObject>(ApplePrefab); // c
+        Apple.transform.position = transform.position; // d
+        Invoke("DropApple", secondsBetweenAppleDrops);
+
+        GameObject greenapple = Instantiate<GameObject>(greenapplePrefab); // c
+        Apple.transform.position = transform.position; // d
+        Invoke("DropApple", secondsBetweenAppleDrops);// e
+    }
 }
+
+
+
+
