57d243d3b91df3edd1b2db403c449a4a7f43130e
dusty1312 <113940912+dusty1312@users.noreply.github.com>
2022-10-05 22:04:42 -0400

    apple tree script connected to prefab

---
+++ Assets/01-Apple Picker/Scripts/appletree.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Appletree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject ApplePrefab;
+    public GameObject greenapplePrefab;
+    // Speed at which the AppleTree moves
+    public float speed = 2f;
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 20f;
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.2f;
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+    void Start()
+    {
+        // Dropping apples every second
+    }
+    void Update()
+    {
+        // Basic Movement
+        // Changing Direction
+    }
+}
